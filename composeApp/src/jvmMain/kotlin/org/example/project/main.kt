1b1edea598dd42d915e5bc050fe8e72b2155ceb3
Emil Flach <emil.flach@jetbrains.com>
2026-04-29 11:29:15 +0200

    Add window state management to main app window
    - Introduced `rememberWindowState` to define window position and size.
    - Set default dimensions to 450x1000dp and positioned window at 50dp (x, y).

--- composeApp/src/jvmMain/kotlin/org/example/project/main.kt
+++ composeApp/src/jvmMain/kotlin/org/example/project/main.kt
@@ -1,13 +1,21 @@
 package org.example.project
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "home-composer",
-        alwaysOnTop = true
+        alwaysOnTop = true,
+        state = rememberWindowState(
+            position = WindowPosition(50.dp, 50.dp),
+            width = 450.dp,
+            height = 1000.dp
+        )
     ) {
         App()
     }
